c86d156487c01fb4c3b137c895f10e84a370a9f4
Riley Manda <rileymanda@Rileys-MacBook-Air.local>
2020-09-17 14:38:53 +0530

    Updates

---
+++ precache-manifest.3868b9e6d032fb2177a2f470ac36b08b.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6153dcb92ceba699bced4745cbf76e2e",
+    "url": "/webshopclient/index.html"
+  },
+  {
+    "revision": "2e931e100d7dec00c014",
+    "url": "/webshopclient/static/css/2.be194de6.chunk.css"
+  },
+  {
+    "revision": "baa794c3c7178dd7ec8a",
+    "url": "/webshopclient/static/css/main.df07c644.chunk.css"
+  },
+  {
+    "revision": "2e931e100d7dec00c014",
+    "url": "/webshopclient/static/js/2.4f42d192.chunk.js"
+  },
+  {
+    "revision": "67e6336f7633796dce809dae6e7fcde8",
+    "url": "/webshopclient/static/js/2.4f42d192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa794c3c7178dd7ec8a",
+    "url": "/webshopclient/static/js/main.9ece4050.chunk.js"
+  },
+  {
+    "revision": "ab19d8f549f30409da8fc8dfe878b463",
+    "url": "/webshopclient/static/js/main.9ece4050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467df629fc06cd00efdb",
+    "url": "/webshopclient/static/js/runtime-main.486c308d.js"
+  },
+  {
+    "revision": "f4684414afa502d0676d17a7d882ce6e",
+    "url": "/webshopclient/static/media/2.f4684414.png"
+  },
+  {
+    "revision": "368685db949cddcd901ba54559a727d9",
+    "url": "/webshopclient/static/media/angular.368685db.jpg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/webshopclient/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/webshopclient/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/webshopclient/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/webshopclient/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/webshopclient/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/webshopclient/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/webshopclient/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/webshopclient/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/webshopclient/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/webshopclient/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/webshopclient/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/webshopclient/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/webshopclient/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/webshopclient/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/webshopclient/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "1e80416aa91f53526b9135b7fbcb8474",
+    "url": "/webshopclient/static/media/facebook.1e80416a.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/webshopclient/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "6f4d93f7ed8b460a11404b0c8af810ad",
+    "url": "/webshopclient/static/media/logo_colour.6f4d93f7.png"
+  },
+  {
+    "revision": "b025e58678b8124951dbd919ac671f9b",
+    "url": "/webshopclient/static/media/logo_white.b025e586.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/webshopclient/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/webshopclient/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/webshopclient/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/webshopclient/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/webshopclient/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "3c2ea25c7dd0aec4d0e92e9553084336",
+    "url": "/webshopclient/static/media/order.3c2ea25c.svg"
+  },
+  {
+    "revision": "e2582654b8e7b6c285d5017df6427d73",
+    "url": "/webshopclient/static/media/profile-cover.e2582654.jpg"
+  },
+  {
+    "revision": "c4eb3d0be0cf5a2d156a123bdf7fb40d",
+    "url": "/webshopclient/static/media/react.c4eb3d0b.jpg"
+  },
+  {
+    "revision": "055d6a90676c6da6efe063a05be8351b",
+    "url": "/webshopclient/static/media/shopping-bag.055d6a90.svg"
+  },
+  {
+    "revision": "e7c82e5c569db9d9761b13ef1384f702",
+    "url": "/webshopclient/static/media/sketch.e7c82e5c.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/webshopclient/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/webshopclient/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "6ee2f7075786ab29687a72cde80092f2",
+    "url": "/webshopclient/static/media/van.6ee2f707.svg"
+  },
+  {
+    "revision": "9378e368a757bd0c7ca556a0258cb527",
+    "url": "/webshopclient/static/media/vue.9378e368.jpg"
+  }
+]);
